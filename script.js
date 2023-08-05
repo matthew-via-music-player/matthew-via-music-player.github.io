f7eb5c5610c99ee459d7d8152d2b97571a3a2b5a
username-matthew <matthew@olk1.com>
2023-08-05 01:16:12 +0100

    loop functionality disabled as not working correctly - stop auto play on load needs to be added

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const audioElement = document.getElementById("audio");
 const playlistElement = document.getElementById("playlist");
-let currentIndex = 0; // To keep track of the current track index
-let tracksData = []; // Store the data array
+const loopAllButton = document.getElementById("loopAllButton");
+const loopButtons = document.querySelectorAll(".loop-track-btn"); 
+let currentIndex = 0; // 
+let tracksData = []; //
+let isLooping = false; 
+let activeLoopButton = null; 
+const trackLoopStates = new Array(tracksData.length).fill(false);
+const shouldStartPlaying = false;
 
-// Fetch JSON data
+// Fetch JSON data and build the player
 fetch("audio_data.json")
   .then(response => response.json())
   .then(data => {
@@ -16,6 +22,20 @@ fetch("audio_data.json")
       titleElement.textContent = item.title;
       titleElement.classList.add("trackName");
       listItem.appendChild(titleElement);
+      
+      const loopButton = document.createElement("button");
+      loopButton.innerHTML = "&infin;";
+      const infinityText = loopButton.textContent;
+      loopButton.textContent = infinityText;
+      loopButton.classList.add("loop-track-btn")
+
+      isLooping
+      ? loopButton.classList.add("loop-track-btn-active")
+      : loopButton.classList.remove("loop-track-btn-active");
+
+      loopButton.setAttribute("data-track-index", index);
+      loopButton.addEventListener("click", toggleLoop);
+      listItem.appendChild(loopButton);
 
       const durationElement = document.createElement("p");
       durationElement.textContent = item.trackDuration;
@@ -32,13 +52,19 @@ fetch("audio_data.json")
   })
   .catch(error => console.error("Error fetching JSON data:", error));
 
-// Function to load track information into the player and play
+
+
+
+
+
+
+
 function loadAndPlayTrack(audioUrl, index) {
   loadTrack(index);
   playAudio(audioUrl);
 }
 
-// Function to load track information into the player
+
 function loadTrack(index) {
   currentIndex = index;
   const tracks = playlistElement.querySelectorAll(".wrapper");
@@ -51,22 +77,100 @@ function loadTrack(index) {
   });
 }
 
-// Function to play audio
-function playAudio(audioUrl) {
-  audioElement.src = audioUrl;
-  audioElement.play();
 
+function playAudio(audioUrl) {
+
+  audioElement.src = audioUrl;  
+
+  // Set loop attribute based on loop state of the current track
+  audioElement.loop = trackLoopStates[currentIndex];
+
+  audioElement.play();
+  
   // Listen for the "ended" event to autoplay the next track
   audioElement.addEventListener("ended", playNextTrack);
 }
+
+
 
 // Function to play the next track
 function playNextTrack() {
   currentIndex++;
   const tracks = playlistElement.querySelectorAll(".wrapper");
-  
-  if (currentIndex < tracks.length) {
-    const nextTrack = tracksData[currentIndex];
-    loadAndPlayTrack(nextTrack.audioUrl, currentIndex);
+
+  if (currentIndex >= tracks.length) {
+    // If the last track has finished playing
+    if (isLooping) {
+      currentIndex = 0;
+    } else {
+      // Stop playback when all tracks have finished (without Loop All)
+      audioElement.pause();
+      return;
+    }
   }
+  
+  const nextTrack = tracksData[currentIndex];
+  loadAndPlayTrack(nextTrack.audioUrl, currentIndex);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function toggleLoop(event) {
+  event.stopPropagation(); // Prevent event propagation to parent elements
+  const loopButton = event.currentTarget;
+  const trackIndex = parseInt(loopButton.getAttribute("data-track-index"));
+
+  loopButton.classList.toggle("loop-track-btn-active");
+
+  // Toggle the loop state for the corresponding track
+  trackLoopStates[trackIndex] = !trackLoopStates[trackIndex];
+
+  // Set the loop attribute based on the loop state of the current track
+  isLooping = trackLoopStates[currentIndex];
+  audioElement.loop = isLooping;
+}
+
+
+
+// Add event listener to individual loop buttons
+loopButtons.forEach(loopButton => {
+  loopButton.addEventListener("click", toggleLoop);
+});
+
